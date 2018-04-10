9bc895e962af549e52d29f957d4183758301622a
Tristan Paust <tristanpaust@gmail.com>
2018-04-09 23:57:42 -0700

    indexed tuples for shuffling the card deck

--- blackjack.hs
+++ blackjack.hs
@@ -1,4 +1,4 @@
-module CardGames where
+module Blackjack where
 
 -- Data Types
 data Suit = Hearts | Diamonds | Clubs | Spades
@@ -54,11 +54,6 @@ fullDeck = allNumbers ++ allFaces ++ allAces
 data Score = Score [Int]
   deriving Show
 
-cardScore :: Card -> Score
-cardScore (RoyalCard v _) = Score [10,0]
-cardScore (AceCard v _) = Score [1,10]
-cardScore (IntCard v _) = Score [v,0]
-
 scoreValue :: Score -> Int
 scoreValue (Score ([])) = 0
 scoreValue (Score (x:xs)) = x + scoreValue (Score xs)
@@ -77,8 +72,6 @@ improveScore (Score (x:xs)) = if (scoreValue (Score (x:xs)) > 21) then (Score [x
 -- scoreValue(Score[11,10])
 -- -> 21
 
-newHand = [IntCard 7 Diamonds, RoyalCard King Hearts, AceCard "Ace" Spades]
-
 instance Monoid Score where
   Score x `mappend` Score y = Score (zipWith (+) x y)
   mempty = Score []
@@ -87,3 +80,59 @@ instance Monoid Score where
 -- Score [2,13] `mappend` Score [10,12] `mappend` Score[3,3]
 -- -> Score [15,28]
 -- TODO: EMPTY WITH NON-EMPTY LIST
+
+-- HANDS FOR TESTING
+notBusted :: Hand
+notBusted = [IntCard 4 Spades, RoyalCard Queen Clubs]
+notBustedIfImproved :: Hand
+notBustedIfImproved = [IntCard 7 Diamonds, RoyalCard King Hearts, AceCard "Ace" Spades]
+definitelyBusted :: Hand
+definitelyBusted = [IntCard 9 Hearts, RoyalCard Jack Clubs, RoyalCard King Spades]
+
+cardScore :: Card -> Score
+cardScore (RoyalCard v _) = Score [10,0]
+cardScore (AceCard v _) = Score [1,10]
+cardScore (IntCard v _) = Score [v,0]
+-- cardScore is a list with two elements, where the first element is the hard score and the second element is the soft score that can be removed
+-- The suit does not influence the cardScore, hence the underscore
+
+-- cardScore (IntCard 7 Diamonds)
+-- -> Score [7,0]
+-- cardScore (AceCard "Ace" Hearts)
+-- -> Score [1,10]
+-- cardScore (RoyalCard Queen Spades)
+-- -> Score [10,0]
+
+handScore :: Hand -> Score
+handScore [] = Score [0,0]
+handScore (x:xs) = cardScore x `mappend` handScore xs
+-- handScore newHand = 7 Diamonds + King Hearts + Ace of Spades = [(7 + 10 + 1), 10] = [18,10]
+-- -> Score [18,10]
+
+handValue :: Hand -> Int
+handValue [] = 0
+handValue x = scoreValue(improveScore(handScore x))
+-- handValue notBusted 
+-- -> 14
+-- handValue notBustedIfImproved
+-- -> 18
+-- handValue definitelyBusted
+-- -> 29
+
+--- Ordered Tuples used for shuffling lists, e.g the full deck of cards
+data Indexed i a = Indexed (i,a)
+  deriving Show
+
+-- Indexed ("a",1)
+-- -> Indexed ("a",1)
+-- Indexed (12412,1)
+-- -> Indexed (12412,1)
+-- Indexed ([1,2,3,4,5],1)
+-- -> Indexed ([1,2,3,4,5],1)
+
+instance (Eq i) => Eq (Indexed i a) where
+  (Indexed (i1,a1)) == (Indexed (i2,a2)) = i1 == i2
+
+instance (Ord i) => Ord (Indexed i a) where
+    (Indexed (i1,a1)) > (Indexed (i2,a2)) = i1 > i2
+    (Indexed (i1,a1)) <= (Indexed (i2,a2)) = i1 <= i2
